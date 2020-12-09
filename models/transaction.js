9af65526e6a4b7e3e769a0c0ae7e90d19136cfe4
Cynthia Mojica <cmojica1231@gmail.com>
2020-12-08 20:53:26 -0800

    modify function

--- models/transaction.js
+++ models/transaction.js
@@ -7,11 +7,11 @@ const transactionSchema = new Schema({
     type: String,
     trim: true,
     required: "Enter a name for transaction"
-  },
+  } 
   value: {
     type: Number,
     required: "Enter an amount"
-  },
+  }
   date: {
     type: Date,
     default: Date.now
